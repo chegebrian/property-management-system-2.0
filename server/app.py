b27ba48b5b21ab0085619b009359c40b380df6e8
brianwaithaka <waithakabrian8@gmail.com>
2026-03-18 05:58:18 +0300

    update app.py

--- server/app.py
+++ server/app.py
@@ -291,6 +291,9 @@ def dashboard():
 
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, port=5555)
+    app.run()
+
+# if __name__ == "__main__":
+#     with app.app_context():
+#         db.create_all()
+#     app.run(debug=True, port=5555)
